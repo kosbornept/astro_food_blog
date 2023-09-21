ff8ca3683f195d8c122d22af8d4f3ca6ed581d5b
Kevin <kwosborne.pt@gmail.com>
2023-09-21 16:32:50 +0100

    Ver 1.2
    GH Pages Site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx()],
+  site: 'https://kosbornept.github.io',
+  base: '/astro_food_blog'
 });
